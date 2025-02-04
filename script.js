d3bec570865956e50f79d21777a094d2871f8b8c
TimurOzer <sareyikama25@gmail.com>
2025-02-04 17:01:01 +0300

    Changing

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const dots = document.querySelectorAll(".dot");
-    
-    dots.forEach((dot, index) => {
-        dot.addEventListener("click", function () {
-            document.querySelector(".dot.active").classList.remove("active");
-            dot.classList.add("active");
-        });
+let currentDot = 0;
+const dots = document.querySelectorAll('.dot');
+const hero = document.querySelector('.hero');
+
+// Arka plan resimlerini tanımla
+const images = [
+    'https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/refs/heads/main/images/test.jpeg',
+    'https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/refs/heads/main/images/otherimage1.jpeg',
+    'https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/refs/heads/main/images/otherimage2.jpeg',
+    'https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/refs/heads/main/images/otherimage3.jpeg'
+];
+
+// Dots'ları tıklanabilir hale getirme
+dots.forEach((dot, index) => {
+    dot.addEventListener("click", function () {
+        // Aktif noktayı güncelle
+        document.querySelector(".dot.active").classList.remove("active");
+        dot.classList.add("active");
+
+        // Arka planı değiştirmek için
+        currentDot = index; // Tıklanan noktaya göre index'i güncelle
+        changeBackground(); // Background'u güncelle
     });
 });
 
-let currentDot = 0;
-const hero = document.querySelector('.hero');
-
-function changeBackground() {
+const changeBackground = () => {
     // Dots'ları güncelle
-    const dots = document.querySelectorAll('.dot');
     dots.forEach(dot => dot.classList.remove('active'));
     dots[currentDot].classList.add('active');
 
-// Arka plan resmini değiştir
-const images = [
-    'https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/main/images/test.jpeg',
-    'https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/main/images/otherimage1.jpeg',
-    'https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/main/images/otherimage2.jpeg',
-    'https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/main/images/otherimage3.jpeg'
-];
-
-hero.style.backgroundImage = `url('${images[currentDot]}')`;
-
-
-    // currentDot'u güncelle
-    currentDot = (currentDot + 1) % dots.length;
-}
-
-// Her 10 saniyede bir değişim yap
-setInterval(changeBackground, 10000);
+    // Arka plan resmini değiştir
+    hero.style.backgroundImage = `url('${images[currentDot]}')`;
+};
 
 // İlk background'u başlat
 changeBackground();
+
+// 10 saniyede bir arka planı değiştir
+const backgroundInterval = setInterval(() => {
+    currentDot = (currentDot + 1) % images.length;
+    changeBackground();
+}, 10000);
