d12d192b3b57cce5cff37d735b4d0472179c22d2
TimurOzer <sareyikama25@gmail.com>
2025-02-04 16:07:35 +0300

    hizmetler

--- script.js
+++ script.js
@@ -8,3 +8,15 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+
+document.getElementById("servicesBtn").addEventListener("click", function(event) {
+    event.stopPropagation(); // Sayfanın başka bir yerine tıklayınca kapanmaması için
+    document.getElementById("dropdownMenu").classList.toggle("show");
+});
+
+// Sayfanın başka bir yerine tıklanınca menüyü kapat
+window.addEventListener("click", function(event) {
+    if (!event.target.matches("#servicesBtn")) {
+        document.getElementById("dropdownMenu").classList.remove("show");
+    }
+});
