06683017d09ff0838e461b0f6fbda4583b651b8e
TimurOzer <sareyikama25@gmail.com>
2025-02-04 15:36:30 +0300

    website

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.getElementById("clickButton");
+    
+    button.addEventListener("click", function () {
+        alert("Button Clicked!");
+    });
+});
