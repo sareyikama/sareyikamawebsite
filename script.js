9a94679a6789dd768ebec71b9841b81ae608c1e2
TimurOzer <sareyikama25@gmail.com>
2025-02-04 16:15:40 +0300

    test

--- script.js
+++ script.js
@@ -20,3 +20,31 @@ window.addEventListener("click", function(event) {
         document.getElementById("dropdownMenu").classList.remove("show");
     }
 });
+
+let currentDot = 0;
+const dots = document.querySelectorAll('.dot');
+const hero = document.querySelector('.hero');
+
+function changeBackground() {
+    // Dots'ları güncelle
+    dots.forEach(dot => dot.classList.remove('active'));
+    dots[currentDot].classList.add('active');
+
+    // Arka plan resmini değiştir
+    const images = [
+        'https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/refs/heads/main/images/test.jpeg',
+        'https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/refs/heads/main/images/otherimage1.jpg',
+        'https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/refs/heads/main/images/otherimage2.jpg',
+        'https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/refs/heads/main/images/otherimage3.jpg'
+    ];
+    hero.style.backgroundImage = `url('${images[currentDot]}')`;
+
+    // currentDot'u güncelle
+    currentDot = (currentDot + 1) % dots.length;
+}
+
+// Her 10 saniyede bir değişim yap
+setInterval(changeBackground, 10000);
+
+// İlk background'u başlat
+changeBackground();
