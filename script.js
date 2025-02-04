69a7f808b3c1d849e3d9e50cd01d2e99b312cda7
TimurOzer <sareyikama25@gmail.com>
2025-02-04 16:21:03 +0300

    imgaes

--- script.js
+++ script.js
@@ -9,34 +9,23 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 });
 
-document.getElementById("servicesBtn").addEventListener("click", function(event) {
-    event.stopPropagation(); // Sayfanın başka bir yerine tıklayınca kapanmaması için
-    document.getElementById("dropdownMenu").classList.toggle("show");
-});
-
-// Sayfanın başka bir yerine tıklanınca menüyü kapat
-window.addEventListener("click", function(event) {
-    if (!event.target.matches("#servicesBtn")) {
-        document.getElementById("dropdownMenu").classList.remove("show");
-    }
-});
-
 let currentDot = 0;
-const dots = document.querySelectorAll('.dot');
 const hero = document.querySelector('.hero');
 
 function changeBackground() {
     // Dots'ları güncelle
+    const dots = document.querySelectorAll('.dot');
     dots.forEach(dot => dot.classList.remove('active'));
     dots[currentDot].classList.add('active');
 
     // Arka plan resmini değiştir
     const images = [
         'https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/refs/heads/main/images/test.jpeg',
-        'https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/refs/heads/main/images/otherimage1.jpg',
-        'https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/refs/heads/main/images/otherimage2.jpg',
-        'https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/refs/heads/main/images/otherimage3.jpg'
+        'https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/refs/heads/main/images/otherimage1.png',
+        'https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/refs/heads/main/images/otherimage2.png',
+        'https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/refs/heads/main/images/otherimage3.png'
     ];
+
     hero.style.backgroundImage = `url('${images[currentDot]}')`;
 
     // currentDot'u güncelle
