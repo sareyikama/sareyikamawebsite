a6a94d2af5a1119cdbcc7b1517da58212525f838
TimurOzer <sareyikama25@gmail.com>
2025-02-04 16:01:11 +0300

    Revert "upload"
    This reverts commit d07dd3986a2d77c3d06863b667b5f8174865a198.

--- script.js
+++ script.js
@@ -8,30 +8,3 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
-
-document.getElementById("servicesBtn").addEventListener("click", function() {
-    document.getElementById("dropdownMenu").classList.toggle("show");
-});
-
-// Sayfanın başka bir yerine tıklanınca menüyü kapat
-window.onclick = function(event) {
-    if (!event.target.matches("#servicesBtn")) {
-        document.getElementById("dropdownMenu").classList.remove("show");
-    }
-};
-
-const backgroundImages = [
-    "https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/refs/heads/main/images/test.jpeg",
-    "https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/refs/heads/main/images/Ekran%20g%C3%B6r%C3%BCnt%C3%BCs%C3%BC%202025-01-20%20141943.png",
-    "https://raw.githubusercontent.com/sareyikama/sareyikamawebsite/refs/heads/main/images/Ekran%20g%C3%B6r%C3%BCnt%C3%BCs%C3%BC%202025-01-21%20175128.png"
-];
-
-let currentIndex = 0;
-
-function changeBackground() {
-    document.body.style.backgroundImage = `url('${backgroundImages[currentIndex]}')`;
-    currentIndex = (currentIndex + 1) % backgroundImages.length;
-}
-
-changeBackground(); // İlk resmi ayarla
-setInterval(changeBackground, 10000); // Her 10 saniyede bir değiştir
